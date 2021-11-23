a2fc6360469dd05acf5e0a81998518801bedabe0
xiezipei <zipeixie@gmail.com>
2021-11-23 18:18:34 +0800

    update: demo 29

---
+++ 29-composition-api/my-vue-app/src/components/counter.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref, computed, onMounted, toRefs, watch } from 'vue'
+
+  // ref响应式变量
+    const count = ref(0)
+    const existCount = ref(6)
+    // computed方法，在count的value发生改变时，会触发计算total
+    const total = computed(() => count.value + existCount.value )
+    function increment() {
+      count.value++
+    }
+    
+    onMounted(() => console.log('component mounted!'))
+
+</script>
+
+<template>
+  <button @click="count++">+1</button>
+  <div>当前todolist事项已有：{{ existCount }}条</div>
+  <div>当前操作已新增：{{ count }}，共有{{ total }}条</div>
+</template>
+
+<style>
+
+</style>
